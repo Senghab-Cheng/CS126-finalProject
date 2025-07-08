cd7da8712dccece9f8a77ab2f279fa1c08a24a17
Chheavang <chheavang123@gmail.com>
2025-07-08 22:03:03 +0700

    Header file

--- pizza.h
+++ pizza.h
@@ -1,4 +1,18 @@
+#include<string>
+#include<vector>
+using namespace std;
 class Pizza{
+    private:
+    string size;
+    vector<string> toppings;                    // An array of strings( string[] )
+    string sauce;
 
-    
+    public:
+    void setSize(string NewSize);
+    string getSize();
+    void addToppings(const string& topping);    // Storing 1 string at a time, 1 after another to a vector
+    vector<string> getToppings();               // A way to return all the toppings
+    void setSauce(string NewSauce);
+    string getSauce();
+    void getPizzaInfo();                        // Prints all info of the pizza out
 };
